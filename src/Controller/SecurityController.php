0a442d95f4fb75a8790cfcb8d4d5b9017a334baf
Willysmile <fabienrabillard@yahoo.fr>
2022-03-26 00:29:30 +0100

    Add Bootstrap files & assets repertory

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Candidates;
+use App\Entity\Recruiters;
+use App\Entity\Consultants;
+use App\Form\CandidatesRegistrationType;
+use App\Form\ConsultantRegistrationType;
+use App\Form\RecruitersRegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -28,5 +37,82 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/inscription-candidats", name="app_registration_candidates")
+     */
+    public function registrationCandidates(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $encoder): Response
+    {
+
+        $user = new Candidates();
+
+        $form = $this->createForm(CandidatesRegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setRoles($user->getRoles());
+            $hash = $encoder->hashPassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/candidates_registration.html.twig', ['form' => $form->createView()]);
+
+    }
+
+    /**
+     * @Route("/inscription-recruteurs", name="app_registration_recruiters")
+     */
+    public function registrationRecruiters(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $encoder): Response
+    {
+
+        $user2 = new Recruiters();
+
+        $form = $this->createForm(RecruitersRegistrationType::class, $user2);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user2->setRoles($user2->getRoles());
+            $hash = $encoder->hashPassword($user2, $user2->getPassword());
+            $user2->setPassword($hash);
+
+            $manager->persist($user2);
+            $manager->flush();
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/recruiters_registration.html.twig', ['form' => $form->createView()]);
+
+    }
+    /**
+     * @Route("/inscription-consultants", name="app_registration_consultants")
+     */
+    public function registrationConsultants(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $encoder): Response
+    {
+
+        $user3 = new Consultants();
+
+        $form = $this->createForm(ConsultantRegistrationType::class, $user3);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user3->setRoles($user3->getRoles());
+            $hash = $encoder->hashPassword($user3, $user3->getPassword());
+            $user3->setPassword($hash);
+
+            $manager->persist($user3);
+            $manager->flush();
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('security/consultant_registration.html.twig', ['form' => $form->createView()]);
+
     }
 }
