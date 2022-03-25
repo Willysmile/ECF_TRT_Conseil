e307aa10b75effee29618e083c28d883c7ff8b99
Willysmile <fabienrabillard@yahoo.fr>
2022-03-25 23:49:19 +0100

    Add Candidates Controller & twig

---
+++ src/Controller/CandidatesController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\JobAds;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CandidatesController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/candidats', name: 'app_candidates')]
+    public function index(): Response
+    {
+        $jobAds = $this->entityManager->getRepository(JobAds::class)->findBy(['IsValidated' => true]);
+
+
+        $user = $this->getUser();
+
+        if ($user && !$user->getIsValidated()) {
+            return $this->render('security/UnValidatedUser.html.twig', []);
+        }
+
+        return $this->render('candidates/index.html.twig', [
+            'controller_name' => 'CandidatesController',
+            'jobAds' => $jobAds
+        ]);
+
+
+    }
+}
