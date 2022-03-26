b9bb3063d0e934d1e3651be9062a5cffada36f7f
Willysmile <fabienrabillard@yahoo.fr>
2022-03-26 20:08:31 +0100

    Add Recruiters Controller

---
+++ src/Controller/RecruitersController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\JobAds;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RecruitersController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
+    #[Route('/recruteurs', name: 'app_recruiters')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+        $jobAds = $this->entityManager->getRepository(JobAds::class)->findBy(['IsValidated' => true, 'recruiters' => $user]);
+
+
+        if ($user && !$user->getIsValidated()) {
+            return $this->render('security/UnValidatedUser.html.twig', []);
+        }
+
+
+        return $this->render('recruiters/index.html.twig', [
+            'controller_name' => 'RecruitersController',
+            'jobAds' => $jobAds
+
+        ]);
+    }
+}
