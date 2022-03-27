9b34c57e5aa5591eb54d62f7a0a74a3b15e2e687
Willysmile <fabienrabillard@yahoo.fr>
2022-03-27 12:16:49 +0200

    Refactoring Candidates & Recruiters controllers

--- src/Controller/CandidatesController.php
+++ src/Controller/CandidatesController.php
@@ -4,10 +4,16 @@ namespace App\Controller;
 
 use App\Entity\JobAds;
 use App\Entity\Recruiters;
+use App\Form\CandidatesChangeInfosType;
+use App\Form\CandidatesChangePasswordType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CandidatesController extends AbstractController
 {
@@ -36,5 +42,71 @@ class CandidatesController extends AbstractController
         ]);
 
 
+    }
+    #[Route('/candidats/compte', name: 'app_candidates_account')]
+    public function index2(Request $request, SluggerInterface $slugger): Response
+    {
+
+        $user = $this->getUser();
+        $form = $this->createForm(CandidatesChangeInfosType::class, $user);
+        $notification = null;
+        $form->handleRequest($request);
+        $CvFile = $form->get('CvFilename')->getData();
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ($CvFile) {
+                $originalFilename = pathinfo($CvFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $CvFile->guessExtension();
+
+                try {
+                    $CvFile->move(
+                        $this->getParameter('cv_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+
+                }
+
+                $user->setCvFilename($newFilename);
+                $this->entityManager->flush();
+                $notification = "Vos informations ont bien étés mises à jour.";
+            }
+        }
+        return $this->render('candidates/account.html.twig', [
+            'controller_name' => 'CandidatesAccountController', 'form' => $form->createView(), 'notification' => $notification, "fileExist" => $CvFile
+        ]);
+
+    }
+
+    #[Route('/candidats/compte/password', name: 'app_candidates_password')]
+    public function index3(Request $request, UserPasswordHasherInterface $encoder): Response
+    {
+
+        $notification = null;
+        $user = $this->getUser();
+        $form = $this->createForm(CandidatesChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $old_pwd = $form->get('old_password')->getData();
+            if ($encoder->isPasswordValid($user, $old_pwd)) {
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $encoder->hashPassword($user, $new_pwd);
+                $user->setPassword($password);
+
+
+                $this->entityManager->flush();
+                $notification = "Votre nouveau mot de passe à bien été mis à jour.";
+            } else {
+                $notification = "Votre mot de passe actuel n’est pas le bon.";
+            }
+
+        }
+        return $this->render('candidates/password.html.twig', [
+            'controller_name' => 'CandidatesAccountController','form' => $form->createView(),
+            'notification' => $notification
+        ]);
     }
 }
--- src/Controller/RecruitersController.php
+++ src/Controller/RecruitersController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\JobAds;
+use App\Form\RecruitersChangeInfosType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,6 +35,34 @@ class RecruitersController extends AbstractController
         return $this->render('recruiters/index.html.twig', [
             'controller_name' => 'RecruitersController',
             'jobAds' => $jobAds
+
+        ]);
+    }
+
+    #[Route('/recruters/compte', name: 'app_recruiters_account')]
+    public function index2(Request $request): Response
+    {
+        $user = $this->getUser();
+        if ($user && !$user->getIsValidated()) {
+            return $this->render('security/UnValidatedUser.html.twig', []);
+        }
+        $form = $this->createForm(RecruitersChangeInfosType::class, $user);
+
+        $notification = null;
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+            $notification = "Vos informations ont bien étés mises à jour.";
+        }
+
+
+        return $this->render('recruiters/account.html.twig', [
+            'controller_name' => 'RecruitersAccountController',
+            'form' => $form->createView(),
+            'notification' => $notification,
 
         ]);
     }
